0da2b6aa1e8635ce6a696613b6ce3f8acec4e794
vincent <wdulkowski@gmail.com>
2022-05-05 12:27:08 +0100

    removed unused file

--- examples_baremetal/example6-pda/rust/src/state.rs
+++
@@ -1,7 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-/// Define the type of state stored in accounts
-#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
-pub struct GreetingAccount {
-    // number of greetings
-    pub counter: u32,
-}
